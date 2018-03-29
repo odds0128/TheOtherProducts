0924aa7232cbf42a10e3aad0be4c54bdce58e5c6
odds0128 <f.r_my-paaaace@docomo.ne.jp>
2018-03-29 21:41:49 +0900

    Tedのtranscriptを綺麗にするプログラム追加

---
+++ cleanTedTranscript.py
@@ -0,0 +1,35 @@
+#! python3
+# cleanTedTranscript.py - クリップボードにコピーしたTedのTranscriptを見やすく直すプログラム
+# Ted ... https://www.ted.com/
+
+
+"""
+wordで作業することを想定して改行文字"¥r"を使っている
+環境に応じて適した改行文字を使うこと
+
+"""
+
+import pyperclip, re
+
+parenthesis_regex = re.compile(r'(\(.*\))?')       # 括弧で括られた文章の正規表現
+time_regex        = re.compile(r'(\d\d\:\d\d)?')   # 時刻の正規表現
+
+text = pyperclip.paste()    # クリップボードの内容をstringで保持
+print(len(text))
+
+lines = text.split('\r')    # textの内容を改行文字で区切ってリストとして保存
+line_num = len(lines)
+new_lines = []
+
+# 以下で各行(リスト"lines"の各要素)について操作
+for line in lines:
+    line = line.strip()
+    # 空行, もしくは時刻だけの行を排除した上で，文章の行の最初をインデントする．
+    if len(time_regex.search(line).group()) == 0 and len(line) > 0:
+        line = parenthesis_regex.sub('', line)   # ()で囲まれた部分の削除
+        new_lines = new_lines + [r' '*2 + line]  # 空白文字2つ分でインデント
+
+text = '\r'.join(new_lines)
+print(len(text))
+
+pyperclip.copy(text)
